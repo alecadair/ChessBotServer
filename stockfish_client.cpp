a0fb2b96148975dfff9639276d702ccc357b860d
Alec S Adair <alec.adair@utah.edu>
2017-08-31 22:38:25 -0600

    changed connection port

--- stockfish_client.cpp
+++ stockfish_client.cpp
@@ -16,14 +16,23 @@ int main(){
     if((socket_fd = socket(AF_INET,SOCK_STREAM, 0)) < 0){
         cout << "could not create socket" << endl;
     }
-    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0){
+    /*if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0){
          cout << "Invalid address" << endl;
+    }*/
+
+    server_addr.sin_family = AF_INET;    
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port =htons(12000);
+    
+    if(inet_aton("127.0.0.1", &server_addr.sin_addr) == 0){
+        perror("could not create address 127.0.0.1");
+        exit(0);
     }
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = 8080;
     if(connect(socket_fd, (struct sockaddr*)&server_addr, 
                              sizeof(server_addr)) < 0){
         cout << "could not connect to server" << endl;
+    }else{
+        cout << "successfully connected socket" << endl;
     }
     return 0;
 }
